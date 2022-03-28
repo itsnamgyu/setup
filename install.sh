2868c007c75a6273622a8f76e93b3ba991f02992
Namgyu Ho <itsnamgyu@gmail.com>
2022-03-28 11:51:19 +0900

    Add git config to install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-# SUPER SIMPLE SETUP SCRIPT.sh
-DIR=$(pwd)
-
 if [ -f "/etc/debian_version" ]; then
 	# Install rust and exa
 	curl https://sh.rustup.rs -sSf | sh -s -- -y
@@ -8,17 +5,17 @@ if [ -f "/etc/debian_version" ]; then
 	unzip exa-linux-x86_64-0.8.0.zip
 	sudo mv exa-linux-x86_64 /usr/local/bin/exa
 	rm exa-linux-x86_64-0.8.0.zip
-
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	# Install exa
 	brew install exa
-
 else
 	# Unsupported platform
 	echo "Only supports debian Linux and macOS."
 	exit
-
 fi
+
+git config --global user.email "itsnamgyu@gmail.com"
+git config --global user.name "Namgyu Ho"
 
 echo "############################ Additional Items ###############################"
 echo "curl -L jupyter-vim.namgyu.io | sh"
