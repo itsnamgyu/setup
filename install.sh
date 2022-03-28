091df41c7c5b687a58a1b146da6d699cb3d12f9e
Namgyu Ho <itsnamgyu@gmail.com>
2022-03-28 11:51:55 +0900

    Add another git config (core.editor)

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ fi
 
 git config --global user.email "itsnamgyu@gmail.com"
 git config --global user.name "Namgyu Ho"
+git config --global core.editor vim
 
 echo "############################ Additional Items ###############################"
 echo "curl -L jupyter-vim.namgyu.io | sh"
