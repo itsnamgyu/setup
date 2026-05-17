c10a78fe31a0fd07d9226e13506cf94837b998bf
Namgyu Ho <itsnamgyu@gmail.com>
2026-05-17 17:00:29 +0900

    Add Claude installation

--- install.sh
+++ install.sh
@@ -2,7 +2,4 @@ git config --global user.email "itsnamgyu@gmail.com"
 git config --global user.name "Namgyu Ho"
 git config --global core.editor vim
 
-echo "############################ Additional Items ###############################"
-echo "curl -L jupyter-vim.namgyu.io | bash"
-echo "https://www.anaconda.com/products/individual#Downloads"
-
+curl -fsSL https://claude.ai/install.sh | bash
